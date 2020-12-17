d3f17b0b03dd9940ebe13c58587423175095b206
shakeeb <kshakeebahmed@gmail.com>
2020-12-17 13:52:34 +0530

    the groceies list app

---
+++ js/login.js
@@ -0,0 +1,55 @@
+function toggleSignup(){
+   document.getElementById("login-toggle").style.backgroundColor="#fff";
+    document.getElementById("login-toggle").style.color="#222";
+    document.getElementById("signup-toggle").style.backgroundColor="#57b846";
+    document.getElementById("signup-toggle").style.color="#fff";
+    document.getElementById("login-form").style.display="none";
+    document.getElementById("signup-form").style.display="block";
+}
+
+function toggleLogin(){
+    document.getElementById("login-toggle").style.backgroundColor="#57B846";
+    document.getElementById("login-toggle").style.color="#fff";
+    document.getElementById("signup-toggle").style.backgroundColor="#fff";
+    document.getElementById("signup-toggle").style.color="#222";
+    document.getElementById("signup-form").style.display="none";
+    document.getElementById("login-form").style.display="block";
+}
+
+
+function store(){
+
+    var name = document.getElementById('name');
+    var pw = document.getElementById('pw');
+
+    if(name.value.length == 0){
+        alert('Please fill in email');
+
+    }else if(pw.value.length == 0){
+        alert('Please fill in password');
+
+    }else if(name.value.length == 0 && pw.value.length == 0){
+        alert('Please fill in email and password');
+
+  }else{
+        localStorage.setItem('name', name.value);
+        localStorage.setItem('pw', pw.value);
+        alert('Success!!!Now Please Login.');
+    }
+}
+
+//checking
+function check(){
+    var storedName = localStorage.getItem('name');
+    var storedPw = localStorage.getItem('pw');
+
+    var userName = document.getElementById('userName');
+    var userPw = document.getElementById('userPw');
+    
+
+    if(userName.value == storedName && userPw.value == storedPw){
+        window.location = "./groceries.html";
+    }else{
+        alert('Unauthorized User');
+    }
+}
